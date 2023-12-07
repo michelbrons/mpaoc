5b2ea1c675f6552a7ceed05eff1799b1fb47e11e
Paalvast <paalvast@paragin.nl>
2023-12-07 16:03:25 +0100

    Added day 8 sample code

---
+++ src/Controller/Day8Controller.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\DayType;
+use App\Service\Day8;
+use App\Service\FileOptions;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class Day8Controller extends AbstractController
+{
+    #[Route('/day7', name: 'app_day7')]
+    public function index(Request $request, FileOptions $fileOptions, Day8 $dayService): Response
+    {
+        $day = 8;
+        $result = '';
+        $form = $this->createForm(DayType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $formData = $form->getData();
+            $rows = $fileOptions->getDayInput($formData, $day);
+
+            if ($formData['day_part'] === 1) {
+                $result = $dayService->generatePart1($rows);
+            } else {
+                $result = $dayService->generatePart2($rows);
+            }
+        }
+
+        return $this->render('day.html.twig', [
+            'day_nr' => $day,
+            'day_title' => '???',
+            'result' => $result,
+            'form' => $form,
+        ]);
+    }
+}
