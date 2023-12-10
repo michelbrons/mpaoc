36281e090859f6ee4c47e97d63e2e586c46bead7
MPaalvast <martijn_paalvast@hotmail.com>
2023-12-10 21:37:38 +0100

    optimizations

--- assets/app.js
+++ assets/app.js
@@ -8,10 +8,10 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
-const $ = require('jquery');
-require('bootstrap');
-
-
-$(document).ready(function() {
-    $('[data-toggle="popover"]').popover();
-});
+// const $ = require('jquery');
+// require('bootstrap');
+//
+//
+// $(document).ready(function() {
+//     $('[data-toggle="popover"]').popover();
+// });
